be27b0f673566599c81a189f176bfcc90fe4b5fd
Abhishek-kumar41 <abhishek.akumar411@gmail.com>
2024-03-20 01:45:30 +0530

    updates: super keyword

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -3,7 +3,7 @@ class Department {
     // private name: string;
     //* The adding of the 'private' access modifier is to make sure that no one is able to execute the code -> accounting.employees[2] = 'anna';
     private employees: string[] = [];
-
+    //* Readonly makes sure that a certain property should only be initialized once and shouldn't change thereafter.
     constructor(private readonly id: string, private name: string) {
     }
 
@@ -22,10 +22,44 @@ class Department {
     }
 }
 
-const accounting = new Department('D1', 'Accounting');
+class ITDepartment extends Department {
+    admins: string[];
+    constructor(id: string, admins: string[]) {
+        super(id, 'IT'); //* Super always needs to be called first, and everything that needs to be done in this class should be after it.
+        this.admins = admins;
+    }
+}
 
-accounting.describe();
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]) {
+        super(id, 'accounting');
+    }
 
-accounting.addEmployee('emp1');
-accounting.printEmployeeInformation();
+    addReports(text: string){
+        this.reports.push(text);
+    }
+
+    getReports() {
+        console.log(this.reports);
+    }
+}
+
+
+// const accounting = new Department('D1', 'Accounting');
+// accounting.describe();
+// accounting.addEmployee('emp1');
+// accounting.printEmployeeInformation();
+
+
+const it = new ITDepartment('IT1', ['max']);
+it.describe();
+it.addEmployee('Max');
+it.addEmployee('Ann');
+it.printEmployeeInformation();
+
+
+const account = new AccountingDepartment('ACC1', []);
+
+account.addReports('something went wrong...');
+account.getReports();
 
