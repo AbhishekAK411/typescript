d242ea4fabf2f96cc1ab383bcf75595ca85f0102
Abhishek-kumar41 <abhishek.akumar411@gmail.com>
2024-03-19 21:58:04 +0530

    updates: access modifiers

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,6 +1,8 @@
 // Code goes here!
 class Department {
     name: string;
+    //* The adding of the 'private' access modifier is to make sure that no one is able to execute the code -> accounting.employees[2] = 'anna';
+    private employees: string[] = [];
 
     constructor(name: string) {
         this.name = name;
@@ -10,9 +12,21 @@ class Department {
     describe(this: Department) {
         console.log('Department ' + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
 
 accounting.describe();
+
+accounting.addEmployee('emp1');
+accounting.printEmployeeInformation();
 
